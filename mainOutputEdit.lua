c895bbe8610b68cf17a5afbe6c803afae5ac72b1
Wretcluse <52970658+Wretcluse@users.noreply.github.com>
2020-03-08 04:14:59 -0600

    Update mainOutputEdit.lua

--- mainOutputEdit.lua
+++ mainOutputEdit.lua
@@ -1,16 +1,15 @@
 function()
-        if aura_env.flag then --prevent iterating everyframe (trigger update might not fire, not sure)
-            aura_env.flag = false
-            aura_env.output = ""
-            for i=1, #aura_env.reflect do
-                --srcName, name, icon, amount, school, critical
-                local target = aura_env.config.target and aura_env.reflect[i][1] or ""
-                local spellName = aura_env.config.spellName and " |cFFbbbbbb["..aura_env.reflect[i][2].."]|r" or ""
-                local icon = aura_env.config.icon and "|T"..aura_env.reflect[i][3]..":0|t" or ""
-                local amount = aura_env.ShortenNumber(aura_env.reflect[i][4])
+    if aura_env.flag then --prevent iterating everyframe (trigger update might not fire, not sure)
+        aura_env.flag = false
+        aura_env.output = ""
+        for i = 1, #aura_env.reflect do
+            --srcName, name, spellName, amount
+            local target = aura_env.config.target and aura_env.reflect[i][1] or ""
+            local spellName = GetSpellLink(spellId) and aura_env.reflect[i][2] or ""
+            local amount = aura_env.ShortenNumber(aura_env.reflect[i][3]) or ""
 
-
-    if aura_env.output then
-        return aura_env.output
+            aura_env.output = aura_env.output..target..spellName..amount
+        end
     end
+    return aura_env.output
 end
