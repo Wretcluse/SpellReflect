6163f413761cc20e3f57b47ca373e246ad1c8863
Wretcluse <52970658+Wretcluse@users.noreply.github.com>
2020-03-09 23:40:16 -0600

    Update SpellReflectEdit.lua

--- SpellReflectEdit.lua
+++ SpellReflectEdit.lua
@@ -57,6 +57,20 @@ function(event, ...)
     end
 end
 
+--[[
+function(event, ...)
+    if event == "COMBAT_LOG_EVENT_UNFILTERED" then
+        local _, message, _, sourceGUID, srcName, _, _, _, destName, _, _, _, _, _, spellId, spellName, _, _, extraSpellId, extraSpellName = ...
+        if message == "SPELL_INTERRUPT" and sourceGUID == UnitGUID("player") then
+            aura_env.target = destName
+            aura_env.spell = GetSpellLink(spellId)
+            aura_env.output = "interrupted " .. aura_env.target .. "'s " .. aura_env.spell .. "!"
+            return true
+        end
+    end
+end
+--]]
+
 
 --[[ 
 REFLECT event Arguments:
